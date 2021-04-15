f1c36c601823f2892f239a5845561908d60c920e
camiloJTG <camilo.montupil@vasslatam.com>
2021-04-14 21:36:28 -0400

    feat: Create index files

---
+++ src/PortfolioApp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AppRouter } from './routes/Index';
+
+export const PortfolioApp = () => {
+   return (
+      <div>
+         <AppRouter />
+      </div>
+   );
+};
