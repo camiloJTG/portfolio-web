daeaa63ff9b9aaf83ee582150ecc155cd4297a95
camiloJTG <camilo.montupil@vasslatam.com>
2021-04-20 23:53:40 -0400

    feat: add process env

---
+++ src/configs/configs.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+   paths: {
+      pathHome: process.env.REACT_APP_PATH_HOME,
+      pathAllSocial: process.env.REACT_APP_PATH_ALL_SOCIAL,
+      pathAllProject: process.env.REACT_APP_PATH_ALL_PROJECT,
+      pathDetailProject: process.env.REACT_APP_PATH_DETAIL_PROJECT,
+      pathProjectByName: process.env.REACT_APP_PATH_PROJECT_BY_NAME
+   },
+   apis: {
+      hostBase: process.env.REACT_APP_HOST_BASE,
+      pathSocial: process.env.REACT_APP_PATH_SOCIAL,
+      pathAccount: process.env.REACT_APP_PATH_ACCOUNT,
+      pathProject: process.env.REACT_APP_PATH_PROJECT
+   }
+};
