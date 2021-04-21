e4d780ffb9591bdf7abf4beb8c2711d6a330f801
camiloJTG <camilo.montupil@vasslatam.com>
2021-04-20 23:22:33 -0400

    feat: Add dark mode in routes

--- src/routes/ClientRoutes.js
+++ src/routes/ClientRoutes.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SwitchChange from '@material-ui/core/Switch';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { Route, Switch } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { DetailProject } from '../pages/DetailProject';
@@ -10,27 +12,40 @@ import { headerClient } from '../constants/headerArrays';
 import config from '../configs/configs';
 
 export const ClientRoutes = () => {
+   const [theme, settheme] = useState(false);
+   const palletType = theme ? 'dark' : 'light';
+   const darkTheme = createMuiTheme({
+      palette: {
+         type: palletType
+      }
+   });
+   const handleThemeChange = () => {
+      settheme(!theme);
+   };
    const { paths } = config;
    return (
       <>
-         <BaseHeader dataItem={headerClient} />
-         <div>
-            <Switch>
-               <Route exact path={paths.pathHome} component={Home} />
-               <Route exact path={`${paths.pathAllProject}/:page`} component={ListProject} />
-               <Route exact path={paths.pathAllSocial} component={SocialMedia} />
-               <Route
-                  exact
-                  path={`${paths.pathProjectByName}/:name/:page`}
-                  component={ListByName}
-               />
-               <Route
-                  exact
-                  path={`${paths.pathDetailProject}/:projectid`}
-                  component={DetailProject}
-               />
-            </Switch>
-         </div>
+         <ThemeProvider theme={darkTheme}>
+            <BaseHeader dataItem={headerClient} />
+            <SwitchChange checked={theme} onChange={handleThemeChange} />
+            <div>
+               <Switch>
+                  <Route exact path={paths.pathHome} component={Home} />
+                  <Route exact path={`${paths.pathAllProject}/:page`} component={ListProject} />
+                  <Route exact path={paths.pathAllSocial} component={SocialMedia} />
+                  <Route
+                     exact
+                     path={`${paths.pathProjectByName}/:name/:page`}
+                     component={ListByName}
+                  />
+                  <Route
+                     exact
+                     path={`${paths.pathDetailProject}/:projectid`}
+                     component={DetailProject}
+                  />
+               </Switch>
+            </div>
+         </ThemeProvider>
       </>
    );
 };
